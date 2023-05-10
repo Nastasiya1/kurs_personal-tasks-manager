d1107da6f932ae302330681da83950281fcb09bc
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-05-10 15:15:40 +0300

    TODOs realization added

--- src/main/java/ru/netology/javacore/Main.java
+++ src/main/java/ru/netology/javacore/Main.java
@@ -5,8 +5,20 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+//        Todos todos = new Todos();
+//        TodoServer server = new TodoServer(8989, todos);
+//        server.start();
+
         Todos todos = new Todos();
-        TodoServer server = new TodoServer(8989, todos);
-        server.start();
+
+        todos.addTask("Сходить в магазин");
+        System.out.println(todos.getAllTasks());
+
+        todos.addTask("Пойти на пробежку");
+        System.out.println(todos.getAllTasks());
+
+        todos.addTask("Йога");
+        todos.removeTask("Пойти на пробежку");
+        System.out.println(todos.getAllTasks());
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,38 @@
 package ru.netology.javacore;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    private final int size = 7;
+    private String[] tasks = new String[size];
 
     public void addTask(String task) {
-        //...
+        for (int i = 0; i < size; i++) {
+            if (tasks[i] == null) {
+                tasks[i] = task;
+                break;
+            }
+        }
     }
 
     public void removeTask(String task) {
-        //...
+        for (int i = 0; i < size; i++) {
+            if (tasks[i] != null && tasks[i].equals(task)) {
+                tasks[i] = null;
+            }
+        }
     }
 
     public String getAllTasks() {
-        //...
-    }
+        List<String> sortedTasks = Arrays.stream(tasks)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.toList());
 
+        return sortedTasks.toString();
+    }
 }
